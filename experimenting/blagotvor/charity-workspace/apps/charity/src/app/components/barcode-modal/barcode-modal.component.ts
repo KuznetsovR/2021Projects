c317c132cf71bfb081b3ce98a22a0e18f61d4b8d
Ruslan <74103648+KuznetsovR@users.noreply.github.com>
2021-09-17 11:11:17 +0500

    moved charity to another repo

--- experimenting/blagotvor/charity-workspace/apps/charity/src/app/components/barcode-modal/barcode-modal.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-barcode-modal',
-	templateUrl: './barcode-modal.component.html',
-	styleUrls: ['./barcode-modal.component.scss']
-})
-export class BarcodeModalComponent {}
